c717732eb50ced09ecad00aa5e25ac7d7b5a43e5
Jigar Gosar <jigar.gosar@gmail.com>
2019-06-29 18:30:30 +0530

    scaffold

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,37 @@ import React from 'react'
 import 'tachyons'
 import './index.css'
 import useGetSet from 'react-use/lib/useGetSetState'
+import equals from 'ramda/es/equals'
+import { taggedSum } from 'daggy'
+import assoc from 'ramda/es/assoc'
+
+const useStateUpdate = initialState => updateFn => {
+  const [get, set] = useGetSet(initialState)
+
+  const send = msg => {
+    const prev = get()
+    const next = updateFn(msg)(prev)
+    if (!equals(prev, next)) {
+      set(next)
+    }
+  }
+
+  return [get(), send]
+}
+
+const Msg = taggedSum('Msg', {
+  OnIpChanged: ['value'],
+})
+
+const update = msg => state => {
+  return msg.cata({
+    OnIpChanged: txt => assoc('ipTxt')(txt)(state),
+  })
+}
 
 export function App() {
-  const [get, set] = useGetSet({ ipTxt: '' })
-  const { ipTxt } = get()
+  const [state, send] = useStateUpdate({ ipTxt: '' })(update)
+  const { ipTxt } = state
   return (
     <div className="pv2 measure-wide center">
       <div>Header</div>
@@ -16,7 +43,7 @@ export function App() {
           autoFocus
           type="text"
           value={ipTxt}
-          onChange={e => set({ ipTxt: e.target.value })}
+          onChange={e => send(Msg.OnIpChanged(e.target.value))}
         />
       </div>
     </div>
