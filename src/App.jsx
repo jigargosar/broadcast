89d1aaef82a509262e5556aecd9b262333b98b0c
Jigar Gosar <jigar.gosar@gmail.com>
2019-06-29 18:10:35 +0530

    scaffold

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import 'tachyons'
+import './index.css'
 
 export function App() {
   return (
-    <div>
+    <div className="measure-wide center">
       <div>Header</div>
       <div>Content</div>
     </div>
