c3559fe17e4d75e508e3c173974d877ec744931f
Jigar Gosar <jigar.gosar@gmail.com>
2019-06-29 18:37:08 +0530

    scaffold

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import useGetSet from 'react-use/lib/useGetSetState'
 import equals from 'ramda/es/equals'
 import { taggedSum } from 'daggy'
 import mergeRight from 'ramda/es/mergeRight'
+import pipe from 'ramda/es/pipe'
+import trim from 'ramda/es/trim'
+import isEmpty from 'ramda/es/isEmpty'
 
 const useStateUpdate = initialState => updateFn => {
   const [get, set] = useGetSet(initialState)
@@ -25,8 +28,19 @@ const Msg = taggedSum('Msg', {
 })
 
 const update = msg => state => {
+  const isBlank = pipe(
+    trim,
+    isEmpty,
+  )
   return msg.cata({
     OnIpChanged: txt => mergeRight(state)({ ipTxt: txt }),
+    OnIpSubmit: () => {
+      if (isBlank(state.ipTxt)) {
+        return state
+      } else {
+        return mergeRight(state)({ ipTxt: '' })
+      }
+    },
   })
 }
 
