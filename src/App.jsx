d84081d51f80d160553b9937c6318b452e5b68ee
Jigar Gosar <jigar.gosar@gmail.com>
2019-06-29 18:17:33 +0530

    scaffold

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import 'tachyons'
 import './index.css'
+import useGetSet from 'react-use/lib/useGetSetState'
 
 export function App() {
+  const [get, set] = useGetSet({ ipTxt: '' })
+  const { ipTxt } = get()
   return (
     <div className="measure-wide center">
       <div>Header</div>
       <div>Content</div>
+      <div className="">
+        <input
+          className="w-100 lh-copy"
+          autoFocus
+          type="text"
+          value={ipTxt}
+          onChange={e => set({ ipTxt: e.target.value })}
+        />
+      </div>
     </div>
   )
 }
